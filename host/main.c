50736f17eb71369647c245eb70aeb3a7646b701b
Woody_K <84436996+woody35545@users.noreply.github.com>
2022-04-26 21:12:04 +0900

    Update main.c

--- host/main.c
+++ host/main.c
@@ -127,7 +127,7 @@ int main(int argc, char *argv[]) // Option을 인자로 받기위해 파라미�
 
 
 	/* Argument 초기화 */
-	if(argc >= 3){
+	if(argc == 4){
 		strcpy(option, argv[1]); //  argv[1] 위치가 option
 		strcpy(context_file_name, argv[2]); // argv[2] 위치가 파일 이름
 		
